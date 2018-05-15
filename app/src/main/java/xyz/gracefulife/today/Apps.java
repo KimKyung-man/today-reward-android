13fa0e069a4e5c0de3c08e745af2203367dc2eda
gracefulife <gracefulife@naver.com>
2018-05-15 23:41:08 +0900

    apps 추가

---
+++ app/src/main/java/xyz/gracefulife/today/Apps.java
@@ -0,0 +1,17 @@
+package xyz.gracefulife.today;
+
+import android.app.Application;
+
+public class Apps extends Application {
+  private static Apps instance;
+
+  @Override public void onCreate() {
+    super.onCreate();
+
+    instance = this;
+  }
+
+  public static Apps get() {
+    return instance;
+  }
+}
